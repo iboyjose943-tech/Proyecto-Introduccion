184431a17a828c6ae1f188cb9a4abd7a0db416e6
Emanuel Iboy <iboyjose943@gmail.com>
2026-05-05 09:08:05 -0600

    codigo completo y corregido

--- Proyecto Introduccion.sql
+++ Proyecto Introduccion.sql
@@ -1,9 +1,45 @@
-CREATE DATABASE PROYECTO
+create database biblioteca
+use biblioteca
 
-USE PROYECTO
-
-CREATE TABLE PRESTAMOSBIBLEOTECARIOS
+Create table Libros 
 (
-  Libro_ID VARCHAR(50),
-  Estudiante_ID VARCHAR(50),
+	LibroID INT PRIMARY	KEY IDENTITY(1, 1),
+	Titulo VARCHAR(150),
+	Autor VARCHAR(100),
+	Editorial VARCHAR(5000),
+	Stock INT NOT NULL
 );
+
+create table Estudiantes 
+(
+	EstudianteID INT PRIMARY KEY IDENTITY(1, 1),
+	Primer_Nombre VARCHAR(150),
+	Apellido VARCHAR(100),
+	Carrera VARCHAR(100),
+	Semestre INT NOT NULL
+);
+
+create table Prestamos_Bibliotecarios
+(
+	PrestamoID INT PRIMARY KEY IDENTITY(1, 1),
+	LibroID INT NOT NULL,
+	EstudianteID INT NOT NULL,
+	Fecha_prestamo Date,
+	Duracion_Prestamo INT
+
+	FOREIGN KEY (LibroID) REFERENCES Libros(LibroID),
+	FOREIGN KEY (EstudianteID) REFERENCES Estudiantes(EstudianteID)
+);
+
+INSERT INTO Libros (Titulo, Autor, Editorial, Stock) VALUES ('Matematicas', 'Baldor', 'susaeta', 50);
+
+INSERT INTO Estudiantes(Primer_Nombre, Apellido, Carrera, Semestre) VALUES ('Juan', 'Juares', 'Medicina', 1);
+
+INSERT INTO Prestamos_Bibliotecarios (LibroID, EstudianteID, Fecha_prestamo, Duracion_Prestamo) VALUES (1, 1, '2026-05-01', 5);
+
+select * from Libros
+
+
+
+delete from Libros where LibroID = 2;
+--drop table Estudiantes--
